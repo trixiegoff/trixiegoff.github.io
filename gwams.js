c622b3198afb03208781b961064b9349fb575d3b
several thousand cats <671259+trixiegoff@users.noreply.github.com>
2025-03-07 11:42:32 -0800

    Update gwams.js

--- gwams.js
+++ gwams.js
@@ -4,26 +4,34 @@ xhr.send();
 
 rawdic = []
 
+msg = function(type, payload) {
+	self.postMessage([type, payload])
+}
+
+log = function(text) {
+	msg(0, text)
+}
+
 xhr.onload = function() {
-  if (xhr.status != 200) { // analyze HTTP status of the response
-    alert(`Error ${xhr.status}: ${xhr.statusText}`); // e.g. 404: Not Found
-  } else { // show the result
-    alert(`Done, got ${xhr.response.length} bytes`); // response is the server response
-		rawdic = xhr.responseText.split("\n")
-		alert(`Loaded ${rawdic.length} words into dic[]`)
+  if (xhr.status != 200) {
+    log(`Error ${xhr.status}: ${xhr.statusText}`);
+  } else {
+    log(`Done, got ${xhr.response.length} bytes`);
+	rawdic = xhr.responseText.split("\n")
+	log(`Loaded ${rawdic.length} words into dic[]`)
   }
 };
 
 xhr.onprogress = function(event) {
   if (event.lengthComputable) {
-    //alert(`Received ${event.loaded} of ${event.total} bytes`);
+    log(`Received ${event.loaded} of ${event.total} bytes`);
   } else {
-    //alert(`Received ${event.loaded} bytes`); // no Content-Length
+    log(`Received ${event.loaded} bytes`);
   }
 
 };
 
 xhr.onerror = function() {
-  alert("Request failed");
+  log("🫠 Failed to load dictionary!");
 };
 
