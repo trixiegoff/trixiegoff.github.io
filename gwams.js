b0b17f3e9b82b8e56d3b059dee204ae6136aa744
several thousand cats <671259+trixiegoff@users.noreply.github.com>
2025-03-07 20:42:01 -0800

    Update gwams.js

--- gwams.js
+++ gwams.js
@@ -1,6 +1,4 @@
-let xhr = new XMLHttpRequest();
-xhr.open('GET', 'words.txt');
-xhr.send();
+
 
 rawdic = []
 
@@ -12,41 +10,44 @@ log = function(text) {
 	msg("log", text)
 }
 
+let xhr = new XMLHttpRequest();
+xhr.open('GET', 'words.txt');
+
 xhr.onload = function() {
   if (xhr.status != 200) {
-    log(`Error ${xhr.status}: ${xhr.statusText}`);
+    log(`Error ${xhr.status}: ${xhr.statusText}`)
   } else {
-    log(`Done, got ${xhr.response.length} bytes`);
+    log(`Done, got ${xhr.response.length} bytes`)
 	rawdic = xhr.responseText.split("\n")
 	log(`Loaded ${rawdic.length} words into dic[]`)
   }
-};
+}
 
 xhr.onprogress = function(event) {
   if (event.lengthComputable) {
-    log(`Received ${event.loaded} of ${event.total} bytes`);
+    log(`Received ${event.loaded} of ${event.total} bytes`)
   } else {
-    log(`Received ${event.loaded} bytes`);
+    log(`Received ${event.loaded} bytes`)
   }
-
-};
+}
 
 xhr.onerror = function() {
   log("🫠 Failed to load dictionary!");
 };
 
+xhr.send()
+
 let hashes = []
-let anagrams = []
+let words = []
 
 
 function weedrun(in_str, out = 0n) {
-  let pos
   for (c of in_str) {
-    let n = BigInt((c.charCodeAt() & ~32) - 65) //lowercase, normalize to a-z = 0-25
-    if (!((n > 26) && (n < 0))) { //only process characters in the english alphabet
-    	pos = 1n << n //set bit n
-    	while((pos & out) == pos) pos = pos << 26n //if bit already set, shift 26 bits, repeat
-    	out |= pos //park that bit
+    let n = BigInt((c.charCodeAt() & ~32) - 65) //lower case & normalize
+    if (!((n > 26) || (n < 0))) { //only process english alphabet
+      n = 1n << n //set bit n
+    	while ((n & out) == n) n <<= 26n //if collision shift up 26 bits
+    	out |= n //park that bit
     }
   }
   return out //return the delicious hashtogram
@@ -56,7 +57,6 @@ function weedrun(in_str, out = 0n) {
 function eatdic(rawdic) {
  	words = rawdic[0].split("\n")
   for (word of words) {
-		//console.log(word)
 		let hash = weedrun(word)
 		let hi = hashes.indexOf(hash)
     if (hi == -1) {
@@ -66,8 +66,6 @@ function eatdic(rawdic) {
 		anagrams[hi] = anagrams[hi] ?? ""
 		anagrams[hi] += word + ","
 	}
-// console.log(anagrams)
-// console.log(hashes.map((x) => x.toString(2)).join("\n\n"))
 }
 
 
@@ -84,7 +82,7 @@ function subtract(hash1, hash2) {
   return hash1
 }
 
-function add(hash1, hash2) {
+function add(hash1, hash2) { //new hotness
   while(hash2 > 0n) {
     smoke = (hash2 ^ hash1)
     hash1 |= hash2
@@ -95,7 +93,7 @@ function add(hash1, hash2) {
 }
 
 
-function add_slow(hash1, hash2) {
+function add_slow(hash1, hash2) { //original bit by bit algorithm
   let pos = (hash1 & -hash1) //get lowest set bit column
   while(pos != 0) {
     hash1 = hash1 & ~pos //unset lowest set bit column
@@ -108,24 +106,8 @@ function add_slow(hash1, hash2) {
   return hash2
 }
 
-function findgwams(hash = 0n) {
-  let gwams = []
-  //hashes.filter((x) => (x & hash) == x).each(())
-	for (hi in hashes) {
-    if ((hash & hashes[hi]) == hashes[hi]) gwams.push(hi)
+function findgwams(hash1 = 0n, gwams = []) {
+  hashes.forEach((hash2, i) => if ((hash1 & hash2) == hash2) gwams.push(hi))
   }
-  //console.log(gwams)
-	
-	gwams.forEach((x) => console.log(anagrams[x]))
   return gwams
 }
-
-//console.log(weedrun(`FOURSCOREANDSEVENYEARSAGOOURFATHERSBROUGHTFORTHONTHISCONTINENTANEWNATIONCONCEIVEDINLIBERTYANDDEDICATEDTOTHEPROPOSITIONTHATALLMENARECREATEDEQUALNOWWEAREENGAGEDINAGREATCIVILWARTESTINGWHETHERTHATNATIONORANYNATIONSOCONCEIVEDANDDEDICATEDCANLONGENDUREWEAREMETONAGREATBATTLEFIELDOFTHATWARWEHAVECOMETODEDICATEAPORTIONOFTHATFIELDASAFINALRESTINGPLACEFORTHOSEWHOHEREGAVETHEIRLIVESTHATTHATNATIONMIGHTLIVEITISALTOGETHERFITTINGANDPROPERTHATWESHOULDDOTHISBUTINALARGERSENSEWECANNOTDEDICATEWECANNOTCONSECRATEWECANNOTHALLOWTHISGROUNDTHEBRAVEMENLIVINGANDDEADWHOSTRUGGLEDHEREHAVECONSECRATEDITFARABOVEOURPOORPOWERTOADDORDETRACTTHEWORLDWILLLITTLENOTENORLONGREMEMBERWHATWESAYHEREBUTITCANNEVERFORGETWHATTHEYDIDHEREITISFORUSTHELIVINGRATHERTOBEDEDICATEDHERETOTHEUNFINISHEDWORKWHICHTHEYWHOFOUGHTHEREHAVETHUSFARSONOBLYADVANCEDITISRATHERFORUSTOBEHEREDEDICATEDTOTHEGREATTASKREMAININGBEFOREUSTHATFROMTHESEHONOREDDEADWETAKEINCREASEDDEVOTIONTOTHATCAUSEFORWHICHTHEYGAVETHELASTFULLMEASUREOFDEVOTIONTHATWEHEREHIGHLYRESOLVETHATTHESEDEADSHALLNOTHAVEDIEDINVAINTHATTHISNATIONUNDERGODSHALLHAVEANEWBIRTHOFFREEDOMANDTHATGOVERNMENTOFTHEPEOPLEBYTHEPEOPLEFORTHEPEOPLESHALLNOTPERISHFROMTHEEARTH`).toString(2))
-
-// a = weedrun("abcz")
-// b = weedrun("abcabcabczzz")
-// console.log([...adhd(a, b).toString(2)].reverse().join(""))
-
-
-//hashes.filter((h) -> h & test == h)
-
